feb600bcfbbcdb0155636eb3ab7514443127e871
Ricky Elrod <ricky@elrod.me>
2017-10-29 21:20:42 -0400

    Add a doc comment
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Types.hs
+++ src/Types.hs
@@ -4,6 +4,10 @@ module Types where
 import Data.BEncode
 import Servant
 
+-- | This type denotes a response that has been bencoded.
+--
+-- We wrap 'BEncode' so we can create Servant instances on it without the use of
+-- orphans, such as 'MimeRender'.
 data BEncodeResponse = BEncodeResponse BEncode deriving (Eq, Show)
 
 instance MimeRender PlainText BEncodeResponse where
